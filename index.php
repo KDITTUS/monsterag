3ed3c210a167af7f669f61088717a2fb8bd3226c
KDITTUS <KristofDittus@web.de>
2017-05-30 10:19:24 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initialscale=1.0">
+    <title>Monster AG</title>
+  </head>
+  <body>
+  </body>
+ </html>
